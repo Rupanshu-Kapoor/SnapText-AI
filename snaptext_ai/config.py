1b9013708b3f2493d1f442f5ab407d8656e79ca9
Rupanshu Kapoor <rupanshukapoor@outlook.com>
2024-07-15 23:54:03 +0530

    Updated rag pipeline to run even for normal query

--- snaptext_ai/config.py
+++ snaptext_ai/config.py
@@ -1,11 +1,24 @@
 
-# model repo 
+# model constants
 mistral_repo = "mistralai/Mistral-7B-Instruct-v0.2"
 llama3_repo = "meta-llama/Meta-Llama-3-8B-Instruct"
 embedding_repo = "BAAI/bge-large-en-v1.5"
 encode_kwargs = {'normalize_embeddings': True}
 
 
+# constants for google cloud documentai
+service_key_path = "google-cloud-service-key.json"
+project_id = "handwritting-recognisation"
+location = "us" 
+processor_id = "4ce6e3828b180cda"
+mime_type = "image/jpeg"    
+field_mask = "text,entities,pages.pageNumber"
+
+mime_types = {
+    ".pdf": "application/pdf",
+    ".jpeg": "image/jpeg",
+    ".jpg": "image/jpeg",
+    ".png": "image/png",}
 
 
 # constants for vector db
@@ -13,3 +26,6 @@ chunk_size = 1000
 chunk_overlap = 200
 top_k = 3
 text = " "
+
+
+UPLOAD_DIRECTORY = "uploads"
--- snaptext_ai/rag_pipeline.py
+++ snaptext_ai/rag_pipeline.py
@@ -6,8 +6,7 @@ from langchain_community.vectorstores import Chroma
 from langchain import hub
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
-from config import *
-
+from .config import *
 
 class RagPipeline:
     def __init__(self):
@@ -15,10 +14,13 @@ class RagPipeline:
         load_dotenv()
         os.environ["HUGGINFACEHUB_API_TOKEN"] = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
-    def rag_pipeline(self, query, need_embedding=False, prompt=None, rag_query=None):
+    def rag_pipeline(self, query, prompt=None, rag_query=None):
 
-        if need_embedding:
-        # creating embeddings 
+        # laoading model
+        llm = HuggingFaceEndpoint(repo_id=mistral_repo, temperature=0.7, max_length=128)
+
+        if rag_query:
+            # creating embeddings 
             hf_embedding = HuggingFaceEmbeddings()
             text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
             documents = text_splitter.split_text(text)
@@ -27,22 +29,22 @@ class RagPipeline:
             chroma_db = Chroma.from_documents(documents, hf_embedding, persist_directory="vecotr_db")
             retriever = chroma_db.as_retriever(search_type="similarity", search_kwargs={"k": top_k})
 
-        # laoading model
-        llm = HuggingFaceEndpoint(repo_id=mistral_repo, temperature=0.7, max_length=128)
-
-        if rag_query:
             prompt = hub.pull("rlm/rag-prompt")
+           
+            def format_docs(docs):
+                return "\n\n".join(doc.page_content for doc in docs)
 
-        def format_docs(docs):
-            return "\n\n".join(doc.page_content for doc in docs)
+            # creating retrieval chain
+            rag_chain = ({
+                "context": retriever | format_docs,
+                "question": RunnablePassthrough()} 
+                | prompt | llm | StrOutputParser()
+            )
 
-        # creating retrieval chain
-        rag_chain = ({
-            "context": retriever | format_docs,
-            "question": RunnablePassthrough()} 
-            | prompt | llm | StrOutputParser()
-        )
+            response = rag_chain.invoke(query)
 
-        response = rag_chain.invoke(query)
-
+        
+        else:
+            response = llm.invoke(query)
+        
         return response
